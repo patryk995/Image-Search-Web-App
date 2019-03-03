0ac4339f92518d85c2c7f3c300217f33347f881a
patryk <patryk.bartosevic@gmail.com>
2019-03-03 19:41:41 +0200

    Updates

---
+++ precache-manifest.02a154398f4f3852d772628499673724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e04cf2ea7bd987ad5c0",
+    "url": "/Image-Search-Web-App/static/js/runtime~main.9e04cf2e.js"
+  },
+  {
+    "revision": "0d3805d5e83fba43a73b",
+    "url": "/Image-Search-Web-App/static/js/main.0d3805d5.chunk.js"
+  },
+  {
+    "revision": "7cb71064deecf1354850",
+    "url": "/Image-Search-Web-App/static/js/2.7cb71064.chunk.js"
+  },
+  {
+    "revision": "0d3805d5e83fba43a73b",
+    "url": "/Image-Search-Web-App/static/css/main.542cd009.chunk.css"
+  },
+  {
+    "revision": "7cb71064deecf1354850",
+    "url": "/Image-Search-Web-App/static/css/2.eacc0552.chunk.css"
+  },
+  {
+    "revision": "ba9edba57de174dede3a4c970cbffb6e",
+    "url": "/Image-Search-Web-App/index.html"
+  }
+];
